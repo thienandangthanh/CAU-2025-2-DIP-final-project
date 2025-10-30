10f6d2cfb5dca873ff355043d6289c50ae78a9d3
Thien An Dang Thanh <24854119+thienandangthanh@users.noreply.github.com>
2025-10-30 14:53:09 +0900

    feat(gui): add Preference dialog

--- gui/dialogs/__init__.py
+++ gui/dialogs/__init__.py
@@ -1,5 +1,6 @@
 """Dialog windows for GUI application."""
 
 from .error_dialog import ErrorDialog
+from .preferences_dialog import PreferencesDialog
 
-__all__ = ["ErrorDialog"]
+__all__ = ["ErrorDialog", "PreferencesDialog"]
